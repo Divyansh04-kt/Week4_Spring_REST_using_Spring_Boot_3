23d8cf64f1bfb337f9fe73bf099f50c09a4c6dfa
Divyansh Katakwar <divyanshkatakwar8@gmail.com>
2025-07-16 01:28:28 +0530

    Create SpringLearnApplication.java

---
+++ 1.Spring Rest Handson/04_Spring Core – Load Country from Spring Configuration XML/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,24 @@
+package com.cognizant.spring_learn;
+
+import com.cognizant.spring_learn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringLearnApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void displayCountry() {
+        LOGGER.info("START: displayCountry()");
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        LOGGER.debug("Country : {}", country.toString());
+        LOGGER.info("END: displayCountry()");
+    }
+
+    public static void main(String[] args) {
+        LOGGER.info("Inside main()");
+        displayCountry();
+    }
+}
