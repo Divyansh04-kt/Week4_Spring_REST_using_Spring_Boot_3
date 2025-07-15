801882818269c1c4ebf5cfa000bbc855c67f3959
Divyansh Katakwar <divyanshkatakwar8@gmail.com>
2025-07-16 01:08:05 +0530

    Update and rename h to JwtUtil.java

---
+++ 5.JWT Handson/spring-learn/src/main/java/com/cognizant/spring_learn/util/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.cognizant.spring_learn.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final long EXPIRATION_TIME = 60 * 60 * 1000; // 1 hour
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+}
+
